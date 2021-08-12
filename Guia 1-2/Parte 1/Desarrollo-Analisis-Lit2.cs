5bc31fd4de0e4b4935d4f07adad50013fffa191f
manuelbma20188 <76193675+manuelbma20188@users.noreply.github.com>
2021-08-11 19:01:36 -0600

    Update Desarrollo-Analisis-Lit2.cs

--- Guia 1-2/Parte 1/Desarrollo-Analisis-Lit2.cs
+++ Guia 1-2/Parte 1/Desarrollo-Analisis-Lit2.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 class CalculoEdad {
     
@@ -11,7 +11,7 @@ class CalculoEdad {
         string anio = fechaActual.Year.ToString();
         
         //hago una conversion del string anterior y se convierte en double para poder realizar el calculo
-	    Double conversion = Convert.ToDouble(anio);
+	Double conversion = Convert.ToDouble(anio);
 	  
         //aca se realiza el propio calculo	  
 	    Double operacion = (conversion - año);
@@ -24,18 +24,30 @@ class CalculoEdad {
         //variable a utilizar
         Double año;
         
+        bool repetir = false; //esta variable me permite validar cuando el user ingresa una fecha mayor al año actual
+        while(repetir !=true){
+            
         Console.WriteLine("\n");
-		Console.WriteLine("\t*------------------------------------------------------------------------*\n");
+	Console.WriteLine("\t*------------------------------------------------------------------------*\n");
         Console.WriteLine("\tBienvenido usuario, este es un programa para calcular tu respectiva edad\n");
         
-	    Console.Write("\tPor favor ingresa tu respectivo año de nacimiento: ");
+	Console.Write("\tPor favor ingresa tu respectivo año de nacimiento: ");
      	año = Convert.ToDouble(Console.ReadLine()); //hacemos la conversion del año ingresado
-     	if(año >2021){
-     	    Console.WriteLine("Fecha de nacimiento no valida");
+     	
+     	if(año <=2021){
+     	    repetir=true;
+            Console.WriteLine("\tTu edad es de: " + CalcularEdad(año) + " años. \n"); //imprimo la funcion pasandole el parametro 
+     	    Console.WriteLine("\t*------------------------------------------------------------------------*\n");   
      	}else{
-     	Console.WriteLine("\tTu edad es de: " + CalcularEdad(año) + " años. \n"); //imprimo la funcion pasandole el parametro 
-     	Console.WriteLine("\t*------------------------------------------------------------------------*\n");    
+     	    Console.Write("\tAño de nacimiento no valido: ");
+     	    //esta funcion hace que se haga una pausa en pantalla, en milisegundos
+            System.Threading.Thread.Sleep(1000);
+     	    Console.Clear();
+            
      	}
-  }
+     	 
+     	
+        }
+    }
   
 }
