f74e18d3699fc5f354315cb42dfa1b2b47b2d8ba
Manuel <bm192191@alumno.udb.edu.sv>
2021-08-22 03:38:58 -0600

    Añadido ejercicio 3

--- Practica 2/Practica 2/Practica 2/Ejercicio3.Designer.cs
+++ Practica 2/Practica 2/Practica 2/Ejercicio3.Designer.cs
@@ -29,21 +29,231 @@ namespace Practica_2
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Ejercicio3));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblX1 = new System.Windows.Forms.Label();
+            this.lblX2 = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Crimson;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(859, -1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(39, 101);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Symbol", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(343, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 32);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Formula cuadrática";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Symbol", 11.25F);
+            this.label2.Location = new System.Drawing.Point(42, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 20);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Symbol", 11.25F);
+            this.label3.Location = new System.Drawing.Point(42, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 20);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Symbol", 11.25F);
+            this.label4.Location = new System.Drawing.Point(42, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 20);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "C";
+            // 
+            // lblX1
+            // 
+            this.lblX1.AutoSize = true;
+            this.lblX1.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblX1.Location = new System.Drawing.Point(42, 308);
+            this.lblX1.Name = "lblX1";
+            this.lblX1.Size = new System.Drawing.Size(14, 21);
+            this.lblX1.TabIndex = 22;
+            this.lblX1.Text = ".";
+            this.lblX1.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // lblX2
+            // 
+            this.lblX2.AutoSize = true;
+            this.lblX2.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblX2.Location = new System.Drawing.Point(42, 351);
+            this.lblX2.Name = "lblX2";
+            this.lblX2.Size = new System.Drawing.Size(14, 21);
+            this.lblX2.TabIndex = 23;
+            this.lblX2.Text = ".";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(98, 113);
+            this.txtA.Multiline = true;
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(147, 21);
+            this.txtA.TabIndex = 24;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(98, 173);
+            this.txtB.Multiline = true;
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(147, 21);
+            this.txtB.TabIndex = 25;
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(98, 224);
+            this.txtC.Multiline = true;
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(147, 21);
+            this.txtC.TabIndex = 26;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.Font = new System.Drawing.Font("Segoe UI Symbol", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.Color.White;
+            this.btnCalcular.Location = new System.Drawing.Point(463, 397);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(329, 38);
+            this.btnCalcular.TabIndex = 27;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(463, 76);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(329, 252);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 28;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LimeGreen;
+            this.button3.Enabled = false;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(873, 193);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(10, 80);
+            this.button3.TabIndex = 29;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LimeGreen;
+            this.button2.Enabled = false;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(873, 292);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(10, 80);
+            this.button2.TabIndex = 30;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.LimeGreen;
+            this.button4.Enabled = false;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.Color.Black;
+            this.button4.Location = new System.Drawing.Point(12, 441);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(10, 80);
+            this.button4.TabIndex = 31;
+            this.button4.UseVisualStyleBackColor = false;
             // 
             // Ejercicio3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(895, 506);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.lblX2);
+            this.Controls.Add(this.lblX1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Ejercicio3";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Ejercicio3";
+            this.Load += new System.EventHandler(this.Ejercicio3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblX1;
+        private System.Windows.Forms.Label lblX2;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
     }
 }
--- Practica 2/Practica 2/Practica 2/Ejercicio3.cs
+++ Practica 2/Practica 2/Practica 2/Ejercicio3.cs
@@ -16,5 +16,83 @@ namespace Practica_2
         {
             InitializeComponent();
         }
+
+        void Limpiar()
+        {
+            txtA.Text = "";
+            txtB.Text = "";
+            txtC.Text = "";
+        }
+        private void Ejercicio3_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+
+        {
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //valido que los txt no queden vacíos
+            if (string.IsNullOrEmpty(txtA.Text) || string.IsNullOrEmpty(txtB.Text) || string.IsNullOrEmpty(txtC.Text))
+            {
+                MessageBox.Show("Verifica que todos los campos estén llenos!");
+            }
+            else
+            {
+                //tomo valores ingresados de txt's
+                string variable1 = txtA.Text.ToString();
+                string variable2 = txtB.Text.ToString();
+                string variable3 = txtC.Text.ToString();
+
+                double conversion1, conversion2, conversion3 = 0;
+                //acá convierto el string a double, si ingresa un valor != a double
+                //mostrará mensaje
+                if (double.TryParse(variable1, out conversion1) && double.TryParse(variable2, out conversion2) && double.TryParse(variable3, out conversion3))
+                {
+                    //calculo primero la discriminante por las condiciones de formula cuadratica
+                    /** Si d >0 hay dos soluciones, 
+                     * si d=0  una sola solucion y 
+                     * si d <0 no hay solucion**/
+
+                    double discriminante = ((Math.Pow(conversion2,2) - (4 * conversion1 * conversion3)));
+                    
+                    if (discriminante > 0)
+                    {
+                        double x1 = (-conversion2 + Math.Sqrt(discriminante)) / (2 * conversion1);                        
+
+                        double x2 = (-conversion2 - Math.Sqrt(discriminante)) / (2 * conversion1);
+                        lblX1.Text = "Resultado de x1: " + x1.ToString();
+                        lblX2.Text = "Resultado de x2: " + x2.ToString();
+                        Limpiar();//metodo que limpia txt's
+                    }
+
+                    if(discriminante == 0)
+                    {
+                        double x1 = (-conversion2 + Math.Sqrt(discriminante)) / (2 * conversion1);
+                        lblX1.Text = "Resultado de x: " + x1.ToString();                        
+                        Limpiar();//metodo que limpia txt's
+                    }
+
+                    if (discriminante < 0)
+                    {
+                        lblX1.Text = "No hay solución";
+                        Limpiar();//metodo que limpia txt's
+                    }                                        
+                }
+                else
+                {
+                    MessageBox.Show("Ingresa un valor valido!");
+                }//fin if de conversion
+            }
+        }
     }
 }
