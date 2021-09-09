2a1d148e4e98fddc616ab8fb276182a28dc7f5a0
Manuel <bm192191@alumno.udb.edu.sv>
2021-09-09 17:51:47 -0600

    Added: Main menu, Excercise 1 [only full validations]

---
+++ Practica-3/Ejercicio1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_3
+{
+    public partial class Ejercicio1 : Form
+    {
+        public Ejercicio1()
+        {
+            InitializeComponent();
+        }
+
+        //function to validate empty txt's
+        bool validarTxtVacio(GroupBox ejercicio1) //instace, type GroupBox
+        {            
+            bool empty=false;
+            foreach (Control c in ejercicio1.Controls) // I check the elements of the groupbox
+            {
+                // if control c is a textbox and it is empty, vacio is gonna be true
+                if (c.GetType() == typeof(TextBox) && c.Text == String.Empty)
+                {
+                    empty = true;
+                }                                       
+            }
+
+            //print a message
+            if (empty == true)
+            {
+                MessageBox.Show("Llena los campos que estén vacios!");
+            }
+
+            return empty;
+        }
+
+        //fuction to validate entrances
+
+        //---validating amount
+        private bool ValidateAmount(double amount)
+        {
+            bool var = false;
+            if(amount ==0)
+            {
+                MessageBox.Show("Ingresa un valor diferente de cero!");
+                txtMonto.Focus();
+                txtMonto.Text = "";
+                var = true;
+            }
+            return var;
+        }
+
+        //validating dui
+        private bool ValidateDui(string duiTxt)
+        {
+            bool var = false;
+            //regular expression to validate DUI entrance
+            //format: ########-#
+            Regex dui = new Regex("^[0-9]{8}-[0-9]{1}$");
+            if (!dui.IsMatch(duiTxt))
+            {
+                MessageBox.Show("Formato de DUI incorrecto");
+                txtDui.Focus();
+                var = true;
+            }
+            return var;
+        }
+
+        //validating nit                
+        private bool ValidateNit(string nitTxt)
+        {
+            bool var = false;
+            //regular expression for NIT entrante
+            //format: ####-######-###-#
+            Regex nit = new Regex("^[0-9]{4}-[0-9]{6}-[0-9]{3}-[0-9]{1}");
+            if (!nit.IsMatch(nitTxt))
+            {
+                MessageBox.Show("Formato de NIT incorrecto");
+                txtNit.Focus();
+                var = true;
+            }
+            return var;                
+        }
+
+        //validating account number's
+        private bool ValidateAccount(string type, string number)
+        {
+            bool var = false;
+            switch (type)
+            {
+                case "Corriente":
+                    //regular expression 
+                    //format: CA-00001
+                    Regex corriente = new Regex("[(CC)]{2}-[0-9]{5}");
+                    if (!corriente.IsMatch(number))
+                    {
+                        MessageBox.Show("Formato de numero de cuenta incorrecto");
+                        txtNumeroCuenta.Focus();
+                        var = true;
+                    }
+                    break;
+
+                case "Ahorros":
+                    //regular expression 
+                    //format: CA-00001
+                    Regex ahorros = new Regex("[(CA)]{2}-[0-9]{5}");
+                    if (!ahorros.IsMatch(number))
+                    {
+                        MessageBox.Show("Formato de numero de cuenta incorrecto");
+                        txtNumeroCuenta.Focus();
+                        var = true;
+                    }
+                    break;
+
+                case "Plazos":
+                    //regular expression 
+                    //format: CA-00001
+                    Regex plazos = new Regex("[(CP)]{2}-[0-9]{5}");
+                    if (!plazos.IsMatch(number))
+                    {
+                        MessageBox.Show("Formato de numero de cuenta incorrecto");
+                        txtNumeroCuenta.Focus();
+                        var = true;
+                    }
+                    break;
+            }
+            return var;
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Ejercicio1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bienvenido!");            //welcome message :)
+            //with this property, the user cant edit the options in cbo
+            cboTipoCuenta.DropDownStyle = ComboBoxStyle.DropDownList;
+            cboSucursal.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            cboTipoCuenta.Items.Clear();
+            cboSucursal.Items.Clear();
+
+            //options for cbo's
+            cboTipoCuenta.Items.Add("Corriente");
+            cboTipoCuenta.Items.Add("Ahorros");
+            cboTipoCuenta.Items.Add("Plazos");
+            cboTipoCuenta.SelectedIndex = 0; //select option 1 by default
+
+            cboSucursal.Items.Add("Sucursal Miguel VCO");
+            cboSucursal.Items.Add("Sucursal Apex SV");
+            cboSucursal.Items.Add("Sucursal Rodríguez SV");
+            cboSucursal.SelectedIndex = 0; //select option 1 by default
+
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {            
+            if (!validarTxtVacio(groupBox1)) //if the function return false, we're gonna make all the proccess
+            {
+                double amount = Convert.ToDouble(txtMonto.Text);
+                string dui = txtDui.Text.ToString();
+                string nit = txtNit.Text.ToString();
+                string type = cboTipoCuenta.SelectedItem.ToString();
+                string numer = txtNumeroCuenta.Text.ToString();
+
+                //same logic, if the functions returns false, we're gonna make all the proccess
+                if (!ValidateAmount(amount) && !ValidateDui(dui) && !ValidateNit(nit) && !ValidateAccount(type, numer))
+                {                    
+                    MessageBox.Show("Cliente registrado satisfactoriamente");
+                }                                                                          
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+        private void txtDui_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        //keypress event, validate txt 'only numbers' acepted
+        private void txtMonto_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            //user can write only one . 
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
---
+++ Practica-3/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); //it close the form
+        }
+
+        //showing the forms
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ejercicio1 ejercicio1 = new Ejercicio1();
+            ejercicio1.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Ejercicio2 ejercicio2 = new Ejercicio2();
+            ejercicio2.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Ejercicio3 ejercicio3 = new Ejercicio3();
+            ejercicio3.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Ejercicio4 ejercicio4 = new Ejercicio4();
+            ejercicio4.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Ejercicio5 ejercicio5 = new Ejercicio5();
+            ejercicio5.Show();
+        }
+    }
+}
